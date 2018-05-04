a2c6e466c47cdbe44055c01d0a032d6e3f2adaca
Jungwoo Park <zerozzung@naver.com>
2018-05-04 11:42:15 +0900

    각 열에 설명을 달았다~

--- kkanbu.c
+++ kkanbu.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // there are many definitions of functions in <stdio.h>
 
-int main(void)
+int main(void) // main function doesn't have input while having int as output type.
 {
-	printf("깐부\n");
-	return 0;
+	printf("깐부\n"); // it prints out "Kkanbu"
+	return 0; // it returns 0 to caller
 }
-
-
